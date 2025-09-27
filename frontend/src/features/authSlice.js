db00e5ac03995c692abcd790c24f6a138f995046
Subhash <subhashdangeti143@gmail.com>
2025-09-28 02:06:50 +0530

    updated Authslice.js

--- frontend/src/features/authSlice.js
+++ frontend/src/features/authSlice.js
@@ -1,5 +1,26 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createSlice } from '@reduxjs/toolkit';
 
-const API_URL = 'http://localhost:5000/api/users';
 const user = JSON.parse(localStorage.getItem('user'));
+
+const initialState = {
+  user: user ? user : null,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setCredentials: (state, action) => {
+      state.user = action.payload;
+      localStorage.setItem('user', JSON.stringify(action.payload));
+    },
+    logout: (state) => {
+      state.user = null;
+      localStorage.removeItem('user');
+    },
+  },
+});
+
+export const { setCredentials, logout } = authSlice.actions;
+export default authSlice.reducer;
+export const selectCurrentUser = (state) => state.auth.user;
