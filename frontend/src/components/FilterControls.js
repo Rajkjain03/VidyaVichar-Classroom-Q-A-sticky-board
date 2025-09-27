a82480555d8cd759aa61f2193198360f73de345f
Subhash <subhashdangeti143@gmail.com>
2025-09-28 02:31:00 +0530

    updated Dashboardpage.js

--- frontend/src/components/FilterControls.js
+++ frontend/src/components/FilterControls.js
@@ -1,16 +1,17 @@
-import React from "react";
+// frontend/src/components/FilterControls.js
+import React from 'react';
 
-function FilterControls({ setFilter, onClear }) {
+const FilterControls = ({ setFilter, onClear }) => {
   return (
     <div className="filter-controls">
       <h3>Instructor Controls</h3>
-      <button onClick={() => setFilter("")}>Show All</button>
-      <button onClick={() => setFilter("unanswered")}>Show Unanswered</button>
-      <button onClick={() => setFilter("answered")}>Show Answered</button>
-      <button onClick={() => setFilter("important")}>Show Important</button>
+      <button onClick={() => setFilter('')}>Show All</button>
+      <button onClick={() => setFilter('unanswered')}>Show Unanswered</button>
+      <button onClick={() => setFilter('answered')}>Show Answered</button>
+      <button onClick={() => setFilter('important')}>Show Important</button>
       <button className="clear-btn" onClick={onClear}>Clear All Questions</button>
     </div>
   );
-}
+};
 
 export default FilterControls;
