18cef8499544ebae45232edf53e57cf5937bc401
QasimANaik <qasim.a.naik00@gmail.com>
2025-09-28 01:14:38 +0530

    created Models

--- backend/src/controllers/questionController.js
+++ backend/src/controllers/questionController.js
@@ -1,43 +1,36 @@
-import Question from "../models/questionModel.js";
+import Question from '../models/questionModel.js';
+import Classroom from '../models/classroomModel.js';
 
-export const getQuestions = async (req, res) => {
-  const filter = req.query.status ? { status: req.query.status } : {};
-  const questions = await Question.find(filter).sort({ createdAt: "desc" });
+export const getQuestionsForClass = async (req, res) => {
+  const questions = await Question.find({ classroom: req.params.classId }).populate('author', 'name').sort({ createdAt: 'desc' });
   res.json(questions);
 };
 
 export const createQuestion = async (req, res) => {
-  const { text, author } = req.body;
-  if (!text || text.trim() === "") {
-    return res.status(400).json({ message: "Question text cannot be empty." });
-  }
-
-  const existingQuestion = await Question.findOne({ text });
-  if (existingQuestion) {
-    return res
-      .status(400)
-      .json({ message: "This question has already been asked." });
-  }
-
-  const question = new Question({ text, author });
+  const { text } = req.body;
+  const question = new Question({
+    text,
+    author: req.user._id,
+    classroom: req.params.classId,
+  });
   const createdQuestion = await question.save();
   res.status(201).json(createdQuestion);
 };
 
 export const updateQuestionStatus = async (req, res) => {
   const { status } = req.body;
-  const question = await Question.findById(req.params.id);
+  const question = await Question.findById(req.params.questionId);
+  const classroom = await Classroom.findById(question.classroom);
+
+  if (req.user._id.toString() !== classroom.teacher.toString()) {
+    return res.status(401).json({ message: 'Only the teacher can update status' });
+  }
 
   if (question) {
-    question.status = status || question.status;
+    question.status = status;
     const updatedQuestion = await question.save();
     res.json(updatedQuestion);
   } else {
-    res.status(404).json({ message: "Question not found" });
+    res.status(404).json({ message: 'Question not found' });
   }
-};
-
-export const clearQuestions = async (req, res) => {
-  await Question.deleteMany({});
-  res.json({ message: "All questions have been cleared." });
 };
